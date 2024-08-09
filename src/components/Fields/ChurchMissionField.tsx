01bc0e5a659fbcb7da17b4ffc1ecd66e4b5a9c46
johnlomat <edwardlomat1503@gmail.com>
2024-08-09 11:37:41 +0800

    Added field

---
+++ src/components/Fields/ChurchMissionField.tsx
@@ -0,0 +1,16 @@
+import FieldWrapper from '@/components/ui/FieldWrapper'
+import { Input } from '@/components/ui/Input'
+import { Label } from '@/components/ui/Label'
+
+const ChurchMissionField = ({ className }: { className?: string }) => {
+  return (
+    <FieldWrapper className={className || ''}>
+      <Label htmlFor="church_mission">
+        Church/Mission<span className="text-red-500"> *</span>
+      </Label>
+      <Input type="text" id="church_mission" />
+    </FieldWrapper>
+  )
+}
+
+export default ChurchMissionField
