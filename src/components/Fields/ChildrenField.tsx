dbb97d7b74db07b80ba1eb5a03c2ef8167bdab37
johnlomat <edwardlomat1503@gmail.com>
2024-08-14 19:05:21 +0800

    Added new field logic

--- src/components/Fields/ChildrenField.tsx
+++ src/components/Fields/ChildrenField.tsx
@@ -10,28 +10,43 @@ import SexField from '@/components/Fields/SexField'
 
 const ChildrenField = ({ className }: FieldProps) => {
   const [isRepeaterOpen, setIsRepeaterOpen] = useState(false)
+  const [repeaterCards, setRepeaterCards] = useState<{ id: number; content: React.ReactNode }[]>([])
 
   const handleClick = () => {
     setIsRepeaterOpen(true)
+
+    const newCard = {
+      id: Date.now(), // Unique identifier
+      content: (
+        <>
+          <SingleField title="Age" description="(at Start of Policy)" type="number" required />
+          <SexField required />
+        </>
+      ),
+    }
+
+    setRepeaterCards((prevCards) => [...prevCards, newCard])
   }
 
-  const handleClickRemove = () => {
-    setIsRepeaterOpen(false)
+  const handleClickRemove = (idToRemove: number) => {
+    setRepeaterCards((prevCards) => prevCards.filter((card) => card.id !== idToRemove))
   }
 
   return (
     <>
-      {isRepeaterOpen && (
-        <RepeaterCard label="Child's Details" onClick={handleClickRemove}>
-          <SingleField title="Age" description="(at Start of Policy)" type="number" required />
-          <SexField required />
-        </RepeaterCard>
-      )}
-      {!isRepeaterOpen && (
-        <FieldWrapper className={className || ''}>
-          <RepeaterButton label="Add Children" onClick={handleClick} />
-        </FieldWrapper>
-      )}
+      {isRepeaterOpen &&
+        repeaterCards.map((repeaterCard, index) => (
+          <RepeaterCard
+            key={repeaterCard.id} // Use the unique id as the key
+            label={`Child's Details #${index + 1}`}
+            onClick={() => handleClickRemove(repeaterCard.id)}
+          >
+            {repeaterCard.content}
+          </RepeaterCard>
+        ))}
+      <FieldWrapper className={className || ''}>
+        <RepeaterButton label="Add Children" onClick={handleClick} />
+      </FieldWrapper>
     </>
   )
 }
